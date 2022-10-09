9d184c8a8a472ddc41e42d5b343061e68e06f4cb
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-10-09 16:43:34 +0200

    infinite lists of primes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
-pub fn print_primes() {
-    for prime in SieveIterator::new().skip(10_000_000).take(20) {
-        println!("{}", prime);
-    }
-}
 
 struct PrimeIterator {
     inner: Option<Box<dyn Iterator<Item = usize>>>,
@@ -58,79 +53,165 @@ fn make_prime_iter_vec() -> impl Iterator<Item = usize> {
     .map(|(i, _)| i)
 }
 
-use bitvec::prelude::*;
+//use bitvec::prelude::*;
 
 struct SieveIterator {
     primes: Vec<usize>,
+    //flags: BitVec<usize, Msb0>,
     flags: Vec<bool>,
     index: usize,
 }
 impl SieveIterator {
-    fn new() -> Self {
-        //let vec = bitvec![usize, Msb0; 0; 1];
+    pub fn new() -> Self {
         SieveIterator {
             primes: Vec::new(),
             flags: vec![false],
             index: 0_usize.wrapping_sub(1),
         }
     }
+
+    fn increase_size(&mut self) {
+        // increase size
+        let old_size = self.flags.len();
+        self.flags.extend((0..old_size * 127).map(|_| true));
+        let new_size = self.flags.len();
+
+        // flag previous primes
+        self.primes.iter().for_each(|&prime| {
+            self.flags
+                .iter_mut()
+                .skip((old_size / prime) * prime - 1)
+                .step_by(prime)
+                .for_each(|b| *b = false);
+        });
+
+        // flag new primes
+        for i in old_size..new_size {
+            if self.flags[i] {
+                let prime = i + 1;
+                self.primes.push(prime);
+                self.flags
+                    .iter_mut()
+                    .skip(prime - 1)
+                    .step_by(prime)
+                    .for_each(|b| *b = false);
+            }
+        }
+    }
+    fn len(&self) -> usize {
+        self.primes.len()
+    }
 }
 impl Iterator for SieveIterator {
     type Item = usize;
+    #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         self.index = self.index.wrapping_add(1);
         match self.primes.get(self.index).copied() {
             Some(p) => Some(p),
             None => {
-                {
-                    // increase size
-                    let old_size = self.flags.len();
-                    self.flags.extend((0..old_size).map(|_| true));
-                    let new_size = self.flags.len();
-
-                    // flag previous primes
-                    self.primes.iter().for_each(|&prime| {
-                        self.flags
-                            .iter_mut()
-                            .skip((old_size / prime) * prime - 1)
-                            .step_by(prime)
-                            .for_each(|b| *b = false);
-                    });
-
-                    // flag new primes
-                    for i in old_size..new_size {
-                        if self.flags[i] {
-                            let prime = i + 1;
-                            self.primes.push(prime);
-                            self.flags
-                                .iter_mut()
-                                .skip(prime - 1)
-                                .step_by(prime)
-                                .for_each(|b| *b = false);
-                        }
-                    }
-                };
+                self.increase_size();
                 self.primes.get(self.index).copied()
             }
         }
     }
+
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        self.index += n;
+        while self.index > self.len() {
+            self.increase_size();
+        }
+        //self.advance_by(n).ok()?;
+        self.next()
+    }
 }
 
+use std::cell::Ref;
+use std::ops::Index;
+
+/// Immutable list created from lazily evaluated iterator
+impl<T> InfiniteList<T>
+where
+    T: Iterator<Item = usize>,
+{
+    fn new(iter: T) -> Self {
+        Self {
+            iter: RefCell::new(iter),
+            values: RefCell::new(Vec::new()),
+        }
+    }
+}
+struct InfiniteList<T: Iterator<Item = usize>> {
+    iter: RefCell<T>,
+    values: RefCell<Vec<usize>>,
+}
+impl<T: Iterator<Item = usize>> Index<usize> for InfiniteList<T> {
+    type Output = usize;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        while index >= self.values.borrow().len() {
+            self.values
+                .borrow_mut()
+                .push(self.iter.borrow_mut().next().unwrap())
+        }
+        //self.values.borrow().get(index).unwrap()
+        unsafe { &*self.values.borrow().as_ptr().offset(index as isize) }
+    }
+}
+
+fn prime_list() -> InfiniteList<SieveIterator> {
+    InfiniteList::new(SieveIterator::new())
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn compare_iterators() {
-        for (((a, b), c), d) in make_prime_iter()
-            .zip(make_prime_iter_vec())
-            .zip(PrimeIterator::new())
-            .zip(SieveIterator::new())
-            .take(1000)
-        {
+    fn make_iters() -> impl Iterator<Item = (((usize, usize), usize), usize)> {
+        Box::new(
+            make_prime_iter()
+                .zip(make_prime_iter_vec())
+                .zip(PrimeIterator::new())
+                .zip(SieveIterator::new()),
+        )
+    }
+
+    fn cmp_iters(iter: Box<dyn Iterator<Item = (((usize, usize), usize), usize)>>) {
+        iter.for_each(|(((a, b), c), d)| {
             assert_eq!(a, b);
             assert_eq!(a, c);
             assert_eq!(a, d);
+        })
+    }
+
+    #[test]
+    fn compare_iterators() {
+        cmp_iters(Box::new(make_iters().take(1000)));
+    }
+    #[test]
+    fn compare_iterators_skip() {
+        for skip in [0, 1, 2, 611, 88, 712, 109, 141, 16, 306, 813, 108, 367] {
+            cmp_iters(Box::new(make_iters().skip(skip).take(50)));
         }
+    }
+
+    #[test]
+    fn infinite_list() {
+        for start in [0, 1, 2, 234] {
+            let iter = (0..).into_iter();
+            let infinite_list = InfiniteList::new(iter);
+            for index in [start, 1, 2, 345, 0, 2348] {
+                assert_eq!(infinite_list[index], index);
+            }
+        }
+    }
+
+    #[test]
+    fn infinite_prime_list() {
+        let prime_list = prime_list();
+
+        assert_eq!(prime_list[3], 7);
+        assert_eq!(prime_list[2], 5);
+        assert_eq!(prime_list[1], 3);
+        assert_eq!(prime_list[0], 2);
     }
 }
--- src/main.rs
+++
@@ -1,4 +0,0 @@
-use prime_iterator_rs::print_primes;
-fn main() {
-    print_primes();
-}
